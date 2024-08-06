7a6fb4f08b40059f617fa8969e48caad1265c187
Víctor Alfonso Tanori Ruiz <victor.tanori@consiss.com>
2024-08-06 15:45:29 -0600

    Upgrade

--- Test/Models/SQLiteDB/Salary.cs
+++ Test/Models/SQLiteDB/Salary.cs
@@ -6,7 +6,6 @@ namespace Test.Models.SQLiteDB
     {
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public Guid SalaryId { get; set; }
         public Guid EmployeeId { get; set; }
         public string PaymentMethod { get; set; } = string.Empty;
         public double MonthlySalary { get; set; }
